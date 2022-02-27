325e60900d7807dc1c090d241ce31795782e18aa
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-27 18:55:49 -0300

    Btn

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
+
+/*-btn*/
+let searchForm = document.querySelector('.shopping-cart');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
