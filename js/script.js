77771a1be926384eb6419d0d232f77bb0bc12ef0
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-27 19:17:20 -0300

    Update shopping-cart btn

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-/*Formulario de pesquisa-btn*/
+/*Search Form/Formulario de pesquisa-btn*/
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
 
-/*-btn*/
-let searchForm = document.querySelector('.shopping-cart');
+/*Shopping-cart/Carrinho de compras-btn*/
+let cart = document.querySelector('.shopping-cart');
 
-document.querySelector('#search-btn').onclick = () => {
-    searchForm.classList.toggle('active');
+document.querySelector('#cart-btn').onclick = () => {
+    cart.classList.toggle('active');
 }
