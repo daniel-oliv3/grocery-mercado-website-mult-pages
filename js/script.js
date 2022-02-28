26ad951a4d7edf7ad273039b9e3de4759df82cc6
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-28 11:47:51 -0300

    Btn login form toggle

--- js/script.js
+++ js/script.js
@@ -5,9 +5,18 @@ document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
 
+
 /*Shopping-cart/Carrinho de compras-btn*/
 let cart = document.querySelector('.shopping-cart');
 
 document.querySelector('#cart-btn').onclick = () => {
     cart.classList.toggle('active');
+}
+
+
+/*Login Form/Formulario de login-btn*/
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
 }
