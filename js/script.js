877466bfe9952d5373fd718f15c6cc05c095af16
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-02 10:30:46 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -44,3 +44,20 @@ window.onscroll = () => {
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
+
+
+/*Next-Slide/Prev-Slide*/
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
